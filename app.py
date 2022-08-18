607dcd2a9cbb77550f8ebf132f7c3cdd970d01c8
MyrandaGoesToSpace <myranda.uselton@gmail.com>
2022-08-18 14:04:26 -0500

    outline app with comments

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.write("""Video Data Processing App!""")
+
+# Import video
+
+# Play video
+
+# If video is paused, save the timestamp
+
+# Input fields
+
+# Show dataframe
